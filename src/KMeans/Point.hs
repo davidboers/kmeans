1fa439513369994c1ca2e32c9d8fd197e571787a
davidboers <kwmmineccraft@gmail.com>
2024-12-14 13:20:44 -0500

    List instances

--- src/KMeans/Point.hs
+++ src/KMeans/Point.hs
@@ -6,7 +6,12 @@ The `Point` class is a type constraint intended to simplify the syntax, specific
 -}
 module KMeans.Point (Point, distance, center) where
 
-import Data.List
+import Data.List as L hiding (head, tail)
+import qualified Data.Text.Lazy as T
+import Data.Text.Metrics (levenshtein)
+import Data.Char
+
+import Data.List.Extras.Argmax
 
 -- | Any observation to be clustered should be of a type with an instance of Point.
 --  
@@ -29,7 +34,7 @@ instance (Eq a, RealFloat a) => Point (a, a) where
       where
         avg :: RealFloat a => [a] -> a
         avg l =
-            sum l / fromIntegral (length l)
+            sum l / fromIntegral (L.length l)
 
     -- | Length of the hypotenouse of the triangle produced between the two coordinates as determined by the Pythagorean Theorem.
     distance (x1, y1) (x2, y2) =
@@ -38,24 +43,46 @@ instance (Eq a, RealFloat a) => Point (a, a) where
             (fromIntegral (round y2) - fromIntegral (round y1))
 
 
--- Lists of integrals
+-- Integrals
 
-instance Integral a => Point [a] where
+instance Point Int where
     -- | Average @point@
+    center = round . avg
+
+    distance x y = fromIntegral $ abs $ x - y
+
+instance Point Char where -- Not technically an integral but whatever
+    center chars = argmax (\c -> length $ filter (c ==) chars) chars
+
+    distance x y = distance (ord x) (ord y)
+
+
+-- Text
+
+instance Point T.Text where
+    center strings =
+        T.pack $ center (map T.unpack strings)
+
+    distance x y = distance (T.unpack x) (T.unpack y) --fromIntegral $ levenshtein (T.toStrict x) (T.toStrict y)
+
+
+-- Lists of anything that can be constrained by `Point`
+
+instance Point a => Point [a] where
     center points
         | all null points = []
-        | otherwise       = map (round . avg) $ transpose points
-      where
-        avg :: Integral a => [a] -> Double
-        avg l =
-            sum (map fromIntegral l) / fromIntegral (length l)
+        | otherwise       = map center $ transpose points
 
     -- | Euclidean distance between the two lists.
     distance xs ys =
-        sqrt . sum $ zipWith (\x y -> squared (fromIntegral x - fromIntegral y)) xs ys
+        sqrt . sum $ zipWith (\x y -> squared (distance x y)) xs ys
 
 
 -- Helpers
+
+avg :: Integral a => [a] -> Double
+avg l =
+    sum (map fromIntegral l) / fromIntegral (L.length l)
 
 hypotenouse :: RealFloat a => a -> a -> a
 hypotenouse a b =
